01e9c7289d590ddb3113b1183e8a063d085e11ab
gds90 <you@example.com>
2024-05-23 10:53:14 +0200

    Push post correzione in aula

--- movie.js
+++ movie.js
@@ -1,15 +1,18 @@
 // Array di film e serie TV
 const mediaArray = [
-    { title: "Dark", year: 2017, genre: "Thriller", rating: 10, type: "tv", seasons: 3 },
-    { title: "Breaking Bad", year: 2008, genre: "Drama", rating: 9.5, type: "tv", seasons: 5 },
+    // movies
     { title: "Pulp Fiction", year: 1994, genre: "Crime", rating: 9, type: "movie" },
-    { title: "Stranger Things", year: 2016, genre: "Horror", rating: 7.5, type: "tv", seasons: 4 },
-    { title: "Game of Thrones", year: 2011, genre: "Fantasy", rating: 8.5, type: "tv", seasons: 8 },
     { title: "Inception", year: 2010, genre: "Action", rating: 8, type: "movie" },
     { title: "The Matrix", year: 1999, genre: "Action", rating: 9, type: "movie" },
     { title: "The Godfather", year: 1972, genre: "Crime", rating: 9, type: "movie" },
     { title: "Shutter Island", year: 2010, genre: "Thriller", rating: 8.5, type: "movie" },
     { title: "The Dark Knight", year: 2008, genre: "Action", rating: 9, type: "movie" },
+
+    // tv series
+    { title: "Dark", year: 2017, genre: "Thriller", rating: 10, type: "tv", seasons: 3 },
+    { title: "Breaking Bad", year: 2008, genre: "Drama", rating: 9.5, type: "tv", seasons: 5 },
+    { title: "Stranger Things", year: 2016, genre: "Horror", rating: 7.5, type: "tv", seasons: 4 },
+    { title: "Game of Thrones", year: 2011, genre: "Fantasy", rating: 8.5, type: "tv", seasons: 8 },
     { title: "Peaky Blinders", year: 2013, genre: "Drama", rating: 8.5, type: "tv", seasons: 6 },
     { title: "Chernobyl", year: 2019, genre: "Drama", rating: 9.5, type: "tv", seasons: 1 }
 ];
@@ -65,17 +68,13 @@ class Movie {
     }
 
     toString() {
-        return `${this.#title} è un film di genere ${this.#genre}. È stato rilasciato nel ${this.#year} ed ha un voto di ${this.#rating}.`
+        return `${this.title} è un film di genere ${this.genre}. È stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}.`
     }
 }
 
 // Classe TvSerie
 class TvSerie extends Movie {
-    #title;
-    #year;
-    #genre;
-    #rating;
-    #type;
+
     #seasons;
 
     constructor(title, year, genre, rating, type, seasons) {
@@ -84,47 +83,17 @@ class TvSerie extends Movie {
     }
 
     // Getter
-    get title() {
-        return this.#title;
-    }
-    get year() {
-        return this.#year;
-    }
-    get genre() {
-        return this.#genre;
-    }
-    get rating() {
-        return this.#rating;
-    }
-    get type() {
-        return this.#type;
-    }
     get seasons() {
         return this.#seasons;
     }
 
     // Setter
-    set title(newTitle) {
-        this.#title = newTitle;
-    }
-    set year(newYear) {
-        this.#year = newYear;
-    }
-    set genre(newGenre) {
-        this.#genre = newGenre;
-    }
-    set rating(newRating) {
-        this.#rating = newRating;
-    }
-    set type(newType) {
-        this.#type = newType;
-    }
     set seasons(newSeasons) {
         this.#seasons = newSeasons;
     }
 
     toString() {
-        return `${this.#title} è una serie tv di genere ${this.#genre}. La prima stagione è stata rilasciata nel ${this.#year} ed in totale sono state prodotte ${this.#seasons} stagioni. Ha un voto di ${this.#rating}.`
+        return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}.`
     }
 }
 
@@ -138,6 +107,15 @@ const mediaArrayMapped = mediaArray.map((media) => {
 });
 
 console.log(mediaArrayMapped);
+
+// Funzione precedente fatta col destructuring durante correzione in aula, evitando di specificare media. per ogni creazione di istanza
+const mediaArrayMapped1 = mediaArray.map(({ title, genre, year, rating, type, seasons }) => {
+    if (type === "movie") {
+        return new Movie(title, year, genre, rating, type);
+    } else {
+        return new TvSerie(title, year, genre, rating, type, seasons);
+    }
+});
 
 // Funzione che restituisca la media dei voti di tutti i film per un determinato genere. 
 function averageRating(mediaArr, genre) {
@@ -158,6 +136,9 @@ function genresList(mediaArr) {
     });
     return genres;
 }
+
+// metodo con reduce:
+// const genres = mediaArray.reduce( (accum, media) => !genres.includes(item.genre) ? [...accum, media.genre] : accum,  []);
 
 console.log(genresList(mediaArray));
 
